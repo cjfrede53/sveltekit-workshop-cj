90c39812e84822c4d1f4fb468e84fb5d63fb1639
Cole Frederickson <cole.frederickson@duke.edu>
2026-01-28 19:01:58 -0500

    Setup button starter code

---
+++ src/lib/Button.svelte
@@ -0,0 +1,16 @@
+<script>
+  export let label = "Button";
+  export let bgColor = "bg-blue-500";
+  export let onClick = () => {};
+</script>
+
+<button
+  class={`px-4 py-2 rounded text-white font-semibold
+          ${bgColor}
+          hover:opacity-80
+          focus:outline-none focus:ring-2 focus:ring-offset-2
+          active:scale-95 transition`}
+  on:click={onClick}
+>
+  {label}
+</button>
