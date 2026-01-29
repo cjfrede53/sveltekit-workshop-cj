0472de959074150ae12e37d9e173173c7ae794a0
Cole Frederickson <cole.frederickson@duke.edu>
2026-01-28 19:04:00 -0500

    Add reusable button component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-"Hello"
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+  import Button from "$lib/Button.svelte";
+
+  function sayHello() {
+    alert("Hello!");
+  }
+
+  function sayGoodbye() {
+    alert("Goodbye!");
+  }
+
+  function saySurprise() {
+    alert("Surprise 🎉");
+  }
+</script>
+
+<div class="flex gap-4 p-8">
+  <Button label="Hello" bgColor="bg-blue-500" onClick={sayHello} />
+  <Button label="Goodbye" bgColor="bg-green-500" onClick={sayGoodbye} />
+  <Button label="Surprise" bgColor="bg-purple-500" onClick={saySurprise} />
+</div>
